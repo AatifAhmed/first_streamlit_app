395adbceb27062987633ee633cb78ac417f0e341
AatifAhmed <54271669+AatifAhmed@users.noreply.github.com>
2023-03-30 16:07:09 +1100

    Update streamlit_app.py
    added df for fruityvice json

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-#new section for fruityvice
+#new section for fruityvice api
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+
+
+#convert to json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#create dataframe
+streamlit.dataframe(fruityvice_normalized)
